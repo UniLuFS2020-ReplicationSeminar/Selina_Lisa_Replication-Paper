23e4159f1353b08d17cfe190f01cc83931ecb17e
SelinaNguyen <62345352+SelinaNguyen@users.noreply.github.com>
2020-04-25 22:31:08 +0200

    Add files via upload
    upload Start.R

---
+++ Syntax/Start.R
@@ -0,0 +1,22 @@
+setwd("/Users/nhatnguyen/Desktop/Uni/FS20/MA Replication/LisaSelina_Replication/Selina_Lisa_Replication-Paper/Syntax")
+
+#Pre-Processing
+
+#Load Data
+
+# load("tydiverse")
+# dat <- load(file = here::here("DATASET DIRECTORY", "DATAFRAME"))
+
+# 1. select key variables -------------------------------------------------
+# 1. select only the key variables
+#   dat <- as_tibble(dat)
+#   select(dat, variable 1, variable 2) #or dat %>% select(variable 1, variable 2)
+
+# 2. drop missing values
+
+# 3. save
+
+#commit 
+
+git config --global user.email "selinadang97@yahoo.de"
+git config --global user.name "Selina Nguyen"
