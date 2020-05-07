142bcd674579e76a652714fe54bea382abb0b0b1
Selina Nguyen <selinadang97@yahoo.de>
2020-05-07 16:40:57 +0200

    tried to normalize the scores

--- Output/Figure1-Senate_Ideal_Points.R
+++ Output/Figure1-Senate_Ideal_Points.R
@@ -28,6 +28,19 @@ ideals <- filter(ideals, congress <= 133,
                  !(domain == "Social" & congress < 85))
 
 # Normalize scores (using Z-score: Z = (x-mean)/sd)
+ideals %>% 
+  group_by(domain) %>% 
+  summarize(mean = mean(dynamic_ideal, na.rm = T), sd = sd(dynamic_ideal, na.rm = T)) %>% 
+  mutate(norm.dynamic_ideal = ((dynamic_ideal - mean) / sd))
+
+ideals %>% 
+  group_by(domain) %>% 
+  summarize(mean = mean(dynamic_ideal, na.rm = T)) %>%
+  mutate(dynamic_ideal = ((ideals$dynamic_ideal - mean) / ideals$dynamic_ideal_sd))
+
+mean(ideals$dynamic_ideal)
+sd(ideals$dynamic_ideal)
+
 # := means update the column dynamic_ideal by the normalized dynamic_ideal
 ideals[, dynamic_ideal := (dynamic_ideal - mean(dynamic_ideal)) / sd(dynamic_ideal),
        by = c("domain")] #Does this mean update column by domain?
